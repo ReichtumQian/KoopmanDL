38ba2548fd192caab3821234847fb1af29c5bbab
reichtum <qyx18267223529@outlook.com>
2024-09-26 13:51:38 +0800

    Fix bugs in FlowMap.

--- KoopmanDL/FlowMap.py
+++ KoopmanDL/FlowMap.py
@@ -8,16 +8,18 @@ class FlowMap(object):
   def step(self, ode, x_init):
     return NotImplementedError
   
-  def generate_traj_data(self, ode, n_init, traj_len, x_min, x_max, seed = None):
+  def generate_traj_data(self, ode, n_init, traj_len, traj_t_step, x_min, x_max, seed = None):
     if seed is None:
       torch.manual_seed(0)
     else:
       torch.manual_seed(seed)
+    n_step = int(traj_t_step / self._dt)
     x = torch.rand(n_init, ode.dim()) * (x_max - x_min) + x_min
     result = x
     for _ in range(traj_len - 1):
-      x = self.step(ode, x)
-      torch.cat([result, x], dim = 0)
+      for __ in range(n_step):
+        x = self.step(ode, x)
+      result = torch.cat([result, x], dim = 0)
     return result
   
     
