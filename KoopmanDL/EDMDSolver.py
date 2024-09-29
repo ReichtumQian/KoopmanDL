e3372f3cb5cae3a28e0a817ac6ff68b170db7f50
reichtum <qyx18267223529@outlook.com>
2024-09-29 21:22:59 +0800

    Add predict part.

--- KoopmanDL/EDMDSolver.py
+++ KoopmanDL/EDMDSolver.py
@@ -17,17 +17,40 @@ class EDMDSolver(object):
     K =  A @ torch.linalg.pinv(G)
     return K.t()
   
-  def eig_decomp(self, K):
-    self.eigenvalues, self.eigenvectors = np.linalg.eig(K.detach().numpy())
-    idx = self.eigenvalues.real.argsort()[::-1]
-    self.eigenvalues = self.eigenvalues[idx]
-    self.eigenvectors = self.eigenvectors[:, idx]
-    self.eigenvectors_inv = np.linalg.inv(self.eigenvectors)
-
-  def eigenfunctions(self, data_x):
-    psi_x = self._dictionary(data_x)
-    val = np.matmul(psi_x, self.eigenvectors)
-    return val
+  def compute_eig(self, K):
+    eigenvalues, eigenvectors = np.linalg.eig(K.detach().numpy())
+    idx = eigenvalues.real.argsort()[::-1]
+    self.eigenvalues = torch.from_numpy(eigenvalues[idx])
+    self.right_eigenvectors = torch.from_numpy(eigenvectors[:, idx])
+    self.left_eigenvectors = torch.from_numpy(np.linalg.inv(eigenvectors))
+  
+  
+  def predict(self, x0, traj_len):
+    M = self._dictionary.get_M()
+    d = x0.size(1)
+    assert(d < M)
+    # Compute matrix B
+    B = torch.zeros(d, M, dtype=torch.cfloat)
+    for i in range(d):
+      B[i, i+1] = 1
+    # Compute W, V
+    W = torch.conj(self.right_eigenvectors)
+    V = B @ W
+    # Compute \Phi
+    def Phi(x):
+      Psi = self._dictionary(x)
+      phi = torch.conj(self.left_eigenvectors.t()) @ Psi.t().to(torch.cfloat)
+      return phi
+      
+    traj = [x0]
+    for _ in range(traj_len - 1):
+      x_current = traj[-1]
+      phi = Phi(x_current)
+      x_next = V @ phi
+      x_next = x_next.to(torch.float).t()
+      traj.append(x_next)
+    return traj
+      
 
 
 class EDMDDLSolver(EDMDSolver):
